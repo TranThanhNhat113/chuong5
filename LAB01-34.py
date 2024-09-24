5863e3dbc1c8201fe3448b610a8d04b3015bf740
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-24 23:34:29 +0700

    Update LAB01-34.py

--- LAB01-34.py
+++ LAB01-34.py
@@ -6,12 +6,12 @@ Created on Sun Sep 15 00:19:12 2024
 """
 
 n = int(input("Nhập vào số nguyên dương n: "))
-if n <= 1:
-    print(f"{n} không phải là số nguyên tố.")
-else:
+if n > 1:
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             print(f"{n} không phải là số nguyên tố.")
             break
     else:
         print(f"{n} là số nguyên tố.")
+else:
+    print(f"{n} không phải là số nguyên tố.")
