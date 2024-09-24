a18f15385355d9878eedbe6c1496b943dffefd0b
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 00:12:00 +0700

    Update LAB01-35.py

--- LAB01-35.py
+++ LAB01-35.py
@@ -7,9 +7,8 @@ Created on Sun Sep 15 00:21:12 2024
 
 n = int(input("Nhập vào số nguyên dương n: "))
 S = 0
-if n > 0:
-    for i in range(1, n + 1):
-        S += i
-    print(f"Tổng S từ 1 đến {n} là: {S}")
-else:
-    print("n không phải là số nguyên dương.")
+while n <= 0:
+    n = int(input("Nhập lại n là số nguyên dương: "))
+for i in range(1, n + 1):
+    S += i
+print(f"Tổng S từ 1 đến {n} là: {S}")
