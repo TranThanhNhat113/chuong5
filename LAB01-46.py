835cd1a5987ac261592b6f30e3c121c7643d493d
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 16:46:40 +0700

    Update LAB01-46.py

--- LAB01-46.py
+++ LAB01-46.py
@@ -5,8 +5,13 @@ Created on Sat Sep 14 23:59:19 2024
 @author: Admin
 """
 
-for x in range(1, 490):
-    for y in range(1, (979 - 2 * x) // 7 + 1):
-        z = (979 - 2 * x - 7 * y) / 9
-        if z > 0 and z.is_integer():
-            print(f"x = {x}, y = {y}, z = {int(z)}")
+danhsach = []
+for x in range(1,490):
+    for y in range(1,140):
+        for z in range(1,109):
+            if 2*x + 7*y + 9*z == 979:
+                danhsach += [(x,y,z)] 
+for i in danhsach:
+      print("bộ nghiệm: ", i)
+while x<=0 and y<=0 and z<=0:
+    print("Không hợp lệ")
