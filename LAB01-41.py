4f80cf36009fd98cd3daa2ea493fa9445933dcb8
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 09:35:08 +0700

    Update LAB01-41.py

--- LAB01-41.py
+++ LAB01-41.py
@@ -5,12 +5,8 @@ Created on Sat Sep 14 23:50:56 2024
 @author: Admin
 """
 
-def tinh_tong(n):
-  tong = 0
-  for i in range(n+1):
-    mau = 2*i + 1
-    tong += 1 / mau
-  return tong
-n = int(input("Nhập số lượng số hạng n: "))
-ket_qua = tinh_tong(n)
-print("Tổng của dãy số là:", ket_qua)
+n = int(input("Nhập giá trị n: "))
+s = 0
+for i in range(n+1):
+  s += 1 / (2*i + 1)
+print("Tổng S(n) =", s)
