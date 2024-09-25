ed055002990d1a6aa2e1069e65212911182987bb
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 09:28:03 +0700

    Update LAB01-38.py

--- LAB01-38.py
+++ LAB01-38.py
@@ -5,11 +5,10 @@ Created on Sun Sep 15 00:25:10 2024
 @author: PC
 """
 
-n = int(input("Nhập vào số nguyên dương lẻ n: "))
-if n > 0 and n % 2 != 0:
-    S = 1
-    for i in range(1, n + 1):
-        S *= i
-    print(f"Tổng S từ 1 đến {n} là: {S}")
-else:
-    print("Số nhập vào phải là số lẻ và lớn hơn 0.")
+n = int(input("Nhập vào số nguyên dương lẻ n để tính(1 * 2 * 3 * ... * n) : "))
+s = 1
+while n <= 0 or n % 2 ==0 :
+    n = int(input("n phải là số nguyên dương lẻ. Nhập lại N: "))
+for i in range (1, n + 1):
+   s = s * i
+print("Kết quả là: ",s)
