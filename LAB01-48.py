51d5cd549b711882268d35f94eefb8ab1ad3c7dd
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 17:01:55 +0700

    Update LAB01-48.py

--- LAB01-48.py
+++ LAB01-48.py
@@ -5,19 +5,18 @@ Created on Sun Sep 15 00:01:56 2024
 @author: Admin
 """
 
-min_sum = float('inf') 
-ds = []  
+danhsach = []
+min_sum = float('inf')
 for x in range(1, 490):
-    for y in range(1, (979 - 2 * x) // 7 + 1):
-        z = (979 - 2 * x - 7 * y) / 9
-        if z > 0 and z.is_integer():
-            z = int(z)
-            tong = x + y + z
-            if tong < min_sum:
-                min_sum, ds = tong, [(x, y, z)]
-            elif tong == min_sum:
-                ds += [(x, y, z)]
-if ds:
-    print(f"Tổng nhỏ nhất x + y + z là: {min_sum}")
-    for x, y, z in ds:
-        print(f"x = {x}, y = {y}, z = {z}")
+    for y in range(1, 140):
+        for z in range(1, 109):
+            if 2 * x + 7 * y + 9 * z == 979:
+                current_sum = x + y + z
+                if current_sum < min_sum:  # Kiểm tra tổng nhỏ nhất
+                    min_sum = current_sum
+                    danhsach = [(x, y, z)]  # Cập nhật danh sách
+if danhsach:
+    x, y, z = danhsach[0]
+    print(f"Nghiệm: x = {x}, y = {y}, z = {z} với tổng x + y + z = {min_sum} nhỏ nhất")
+else:
+    print("Không tìm thấy nghiệm nào.")
