ae687e871d11102821f68728c762b3ce8306a300
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 11:13:28 +0700

    Update LAB01-48.py

--- LAB01-48.py
+++ LAB01-48.py
@@ -6,18 +6,18 @@ Created on Sun Sep 15 00:01:56 2024
 """
 
 min_sum = float('inf') 
-solutions = []  
+ds = []  
 for x in range(1, 490):
     for y in range(1, (979 - 2 * x) // 7 + 1):
         z = (979 - 2 * x - 7 * y) / 9
         if z > 0 and z.is_integer():
             z = int(z)
-            current_sum = x + y + z
-            if current_sum < min_sum:
-                min_sum, solutions = current_sum, [(x, y, z)]
-            elif current_sum == min_sum:
-                solutions += [(x, y, z)]
-if solutions:
+            tong = x + y + z
+            if tong < min_sum:
+                min_sum, ds = tong, [(x, y, z)]
+            elif tong == min_sum:
+                ds += [(x, y, z)]
+if ds:
     print(f"Tổng nhỏ nhất x + y + z là: {min_sum}")
-    for sol in solutions:
-        print(f"x = {sol[0]}, y = {sol[1]}, z = {sol[2]}")
+    for x, y, z in ds:
+        print(f"x = {x}, y = {y}, z = {z}")
