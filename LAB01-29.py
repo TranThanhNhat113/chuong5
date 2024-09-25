772d2f4d553fd22144092c90f12594fd57720728
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 08:59:02 +0700

    Update LAB01-29.py

--- LAB01-29.py
+++ LAB01-29.py
@@ -6,6 +6,8 @@ Created on Sat Sep 14 23:58:45 2024
 """
 
 N = int(input("Nhập vào một số nguyên dương N: "))
+while N <= 0:
+    N = int(input("N phải là số nguyên dương. Nhập lại N: "))
 tong = 0
 while N > 0:
     tong += N % 10
