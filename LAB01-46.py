a9ca9fbe1f43b319d80ea2a3730f79edd02bb9b0
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 10:51:24 +0700

    Update LAB01-46.py

--- LAB01-46.py
+++ LAB01-46.py
@@ -5,14 +5,8 @@ Created on Sat Sep 14 23:59:19 2024
 @author: Admin
 """
 
-a, b, c, n = 2, 7, 9, 979
-solutions = []
-for x in range(1, n // a + 1):
-    for y in range(1, (n - a * x) // b + 1):
-        if (n - a * x - b * y) % c == 0:
-            z = (n - a * x - b * y) // c
-            if z > 0:
-                solutions += [(x, y, z)]
-print("Các bộ nghiệm nguyên của phương trình là:")
-for sol in solutions:
-    print(sol)
+for x in range(1, 490):
+    for y in range(1, (979 - 2 * x) // 7 + 1):
+        z = (979 - 2 * x - 7 * y) / 9
+        if z > 0 and z.is_integer():
+            print(f"x = {x}, y = {y}, z = {int(z)}")
