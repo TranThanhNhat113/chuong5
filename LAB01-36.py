404a139fd5786d1da9d56dc871768ddb6abfb457
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 09:12:01 +0700

    Update LAB01-36.py

--- LAB01-36.py
+++ LAB01-36.py
@@ -6,10 +6,10 @@ Created on Sun Sep 15 00:22:36 2024
 """
 
 n = int(input("Nhập vào số nguyên dương n: "))
-s = 0
+S = 0
 while n <= 0:
     n = int(input("n phải là số nguyên dương. Nhập lại n: "))
 for i in range (1, n + 1):
-   s += i ** 2
+   S += i ** 2
 print(f"Tổng S từ 1^2 đến {n}^2 là: {S}")
     
