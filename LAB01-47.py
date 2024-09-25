bbf337dd2437d2b318cbdccc57db79fcab62c1de
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 16:58:13 +0700

    Update LAB01-47.py

--- LAB01-47.py
+++ LAB01-47.py
@@ -6,14 +6,17 @@ Created on Sat Sep 14 23:59:58 2024
 """
 
 danhsach = []
-max = 0
+max_sum = 0
 for x in range(1,490):
     for y in range(1,140):
         for z in range(1,109):
             if 2*x + 7*y + 9*z == 979:
-                sum= x+y+z
-                if sum > max:
-                    max = sum
+                current_sum= x+y+z
+                if current_sum > max_sum:
+                    max_sum = current_sum
                     danhsach = [(x,y,z)]
-print(f"{danhsach} voi bo nghiem {x+y+z}={max}")
-            
+if danhsach:
+    x, y, z = danhsach[0]
+    print(f"Nghiệm: x = {x}, y = {y}, z = {z} với tổng x + y + z = {max_sum}")
+else:
+    print("Không tìm thấy nghiệm nào.")
