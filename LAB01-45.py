beba364bae41585e394745becfae156be355c4b9
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 10:28:29 +0700

    Update LAB01-45.py

--- LAB01-45.py
+++ LAB01-45.py
@@ -7,9 +7,9 @@ Created on Sat Sep 14 23:57:24 2024
 
 x = float(input("Nhập giá trị x: "))
 n = int(input("Nhập giá trị n: "))
-S = x
-mau_so = 1
-for i in range(2, n+1):
+S = 0
+mau_so = 0
+for i in range(1, n+1):
   mau_so += i
   S += x**i / mau_so
 print("Tổng S(n) =", S)
