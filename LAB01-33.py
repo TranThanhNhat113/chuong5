acc9c0f1381bbbf0f23c7012830824e493d7bee1
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 09:01:38 +0700

    Update LAB01-33.py

--- LAB01-33.py
+++ LAB01-33.py
@@ -7,10 +7,9 @@ Created on Sun Sep 15 00:15:15 2024
 
 import math
 n = int(input("Nhập vào số nguyên dương n: "))
-if n>0:
-    if int(math.sqrt(n)) ** 2 == n:
-        print(f"{n} là số chính phương")
-    else:
-        print(f"{n} không phải là số chính phương")
+while n <= 0:
+    n = int(input("n phải là số nguyên dương. Nhập lại n: "))
+if int(math.sqrt(n)) ** 2 == n:
+    print(f"{n} là số chính phương")
 else:
-    print("n không phải là số nguyên dương")
+    print(f"{n} không phải là số chính phương")
