b27fb0941556316f8fd18af67abcf5f3f283fd5d
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 09:07:13 +0700

    Update LAB01-34.py

--- LAB01-34.py
+++ LAB01-34.py
@@ -6,6 +6,8 @@ Created on Sun Sep 15 00:19:12 2024
 """
 
 n = int(input("Nhập vào số nguyên dương n: "))
+while n <= 0:
+    n = int(input("n phải là số nguyên dương. Nhập lại n: "))
 if n > 1:
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
