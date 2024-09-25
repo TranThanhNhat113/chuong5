5dfc8a9ac4c99f4a72a88acb587aa3457012488a
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-25 16:25:15 +0700

    Update LAB01-47.py

--- LAB01-47.py
+++ LAB01-47.py
@@ -5,19 +5,15 @@ Created on Sat Sep 14 23:59:58 2024
 @author: Admin
 """
 
-solutions = []
-
-for x in range(1, 490):
-    for y in range(1, (979 - 2 * x) // 7 + 1):
-        z = (979 - 2 * x - 7 * y) / 9
-        if z > 0 and z.is_integer():
-            z = int(z)
-            solutions += [(x, y, z)]  # Thay thế append bằng toán tử cộng
-
-# Tìm tổng lớn nhất và in ra kết quả
-if solutions:
-    max_sum = max(sum(solution) for solution in solutions)
-    print(f"Tổng lớn nhất x + y + z là: {max_sum}")
-    for x, y, z in solutions:
-        if x + y + z == max_sum:
-            print(f"x = {x}, y = {y}, z = {z}")
+danhsach = []
+max = 0
+for x in range(1,490):
+    for y in range(1,140):
+        for z in range(1,109):
+            if 2*x + 7*y + 9*z == 979:
+                sum= x+y+z
+                if sum > max:
+                    max = sum
+                    danhsach = [(x,y,z)]
+print(f"{danhsach} voi bo nghiem {x+y+z}={max}")
+            
